7002114fe452550b6d86e4b9be6a8b1614dc4547
rmena014 <rmena014@gmail.com>
2022-10-06 21:55:29 -0400

    Added Sidenav, new logos, removed some files

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 7000); //  image changes every 7 seconds
 }
+//Side nav Javascript
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
