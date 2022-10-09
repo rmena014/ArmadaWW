85cfa661990b33f5c28f8f8f9e72a718598f357f
rmena014 <rmena014@gmail.com>
2022-10-09 01:50:51 -0400

    Added Product Page Images

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-//Homepage Slider javascript
+//first we open up a new object, request
+let request = new XMLHttpRequest();
+//Fetches the products.json file
+request.open("get", "products.json", true);
+//sends a request
+request.send();
+//telling the function to run when page loads
+request.onload = function () {
+  let products = JSON.parse(this.responseText);
+  let output = " ";
+  for (let item of products) {
+    output += `<section>
+    <div class="wrapper">
+      <div class="productimg"><img src="${item.image}"</div>
+      <div class="productinfo"> 
+        <div class="productText">
+          <h1 class="productTitle">${item.clothesId}</h1>
+          <p class="productDesc">${item.description}</p>
+        </div>
+        <div class="productBuy">
+          <p class="price">${item.price}</p>
+          <button class="addtocart" type="button">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </section>`;
+  }
+  document.querySelector(".wrapper").innerHTML = output;
+};
 
+//Homepage Slider javascript
 let slideIndex = 0;
 showSlides();
 
